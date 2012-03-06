7acfa175bed89e073a3687bd8c9b30a35358c909
DJ HB <dj2@djhb.local>
2012-03-05 20:58:49 -0500

    added first files

---
+++ Hudson_Bryan_Assignment1.js
@@ -0,0 +1,28 @@
+<script>
+var Buyer = "Richie Rich";
+var Vehicle1 = "Mercedes";
+var Vehicle2 = "Ferrari";
+var vehicle3 = "Maserati";
+
+
+var 'buy' = true 
+var 'don\'t buy' = false 
+ 
+
+
+print "Wecome to Classy Imports.";
+print " Would you like to view our inventory?";
+if vehicle1 <='50,000' = "buy";
+    Else "don't buy";
+if vehicle2 <='50,000' = "buy";
+    Else "don't buy";
+if vehicle3 <='50,000' = "buy";
+    Else "don't buy";
+End if;
+console.log  "Thank you for your Purchase!";
+<script/>
+
+
+
+
+//Bryan Hudson, 3-1-12, Deliverable 1
